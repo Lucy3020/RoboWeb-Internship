153211ea9fdfecbdbc5aaf695739504f18235d20
Lucy3020 <67004463+Lucy3020@users.noreply.github.com>
2022-09-07 12:36:39 +0700

    Update App.js

--- my-app/intern/src/App.js
+++ my-app/intern/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Link, Route} from "react-router-dom";
+import Home from './components/Home';
+import About from './components/About';
+import React, { Component } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/Lucy3020/RoboWeb-Internship.git"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <h1>Trang chu</h1>
+        <Routes>
+          <Route exact path='/' element={<Home />}></Route>
+          <Route exact path='/about' element={<About />}></Route>
+          <Route exact path='/home' element={<Home />}></Route>
+        </Routes>
+      </div>
+  </Router>
+   
   );
 }
 
